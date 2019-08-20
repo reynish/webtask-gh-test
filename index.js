73ddedf1281fce527fc9f512d031f23f7ff6bfa9
Alex Reynish <reynish@gmail.com>
2019-08-20 09:50:45 +0100

    Updating index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Slack = require('slack');
 module.exports = function(context, cb) {
     const token = context.secrets.TOKEN;
     const bot = new Slack({ token });
-    console.log(bot.channels.list({ token: token }));
+    console.log(bot.channels.list({ token: token }).then(console.log));
     bot.chat
         .postMessage({ text: 'blah', channel: 'food-bot' })
         .then(console.log)
